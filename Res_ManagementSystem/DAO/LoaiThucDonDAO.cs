81b929a7004daff222ab6c6148514e5e183f98b9
KenBaro <79701258+kenbaro@users.noreply.github.com>
2021-11-13 22:12:46 +0700

    Update LoaiThucDonDAO.cs

--- Res_ManagementSystem/DAO/LoaiThucDonDAO.cs
+++ Res_ManagementSystem/DAO/LoaiThucDonDAO.cs
@@ -1,7 +1,8 @@
-﻿using Res_ManagementSystem.DTO;
+using Res_ManagementSystem.DTO;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,18 @@ namespace Res_ManagementSystem.DAO
         public static List<LoaiThucDonDTO> LayDSLoaiThucDon()
         {
             List<LoaiThucDonDTO> _ds = new List<LoaiThucDonDTO>();
-            string sql = "select * from QLYQUANNHAU.[dbo].[LoaiThucDon]";
-            DataTable dt = DataProvider.ExecuteQuery(sql);
+            /*string sql = "select * from QLYQUANNHAU.[dbo].[LoaiThucDon]";
+            DataTable dt = DataProvider.ExecuteQuery(sql)*/;
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[LayDSLoaiThucDon]";
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             for (int i = 0; i < dt.Rows.Count; i++)
             {
                 LoaiThucDonDTO loai = new LoaiThucDonDTO();
