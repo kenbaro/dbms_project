d2b55bd4fed09d3d3b572e8d8e470dd6ce3404cd
KenBaro <79701258+kenbaro@users.noreply.github.com>
2021-11-13 22:13:09 +0700

    Update NhanVienDAO.cs

--- Res_ManagementSystem/DAO/NhanVienDAO.cs
+++ Res_ManagementSystem/DAO/NhanVienDAO.cs
@@ -1,4 +1,4 @@
-﻿using Res_ManagementSystem.DTO;
+using Res_ManagementSystem.DTO;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -11,6 +11,7 @@ namespace Res_ManagementSystem.DAO
 {
     public class NhanVienDAO
     {
+        
         public static int MaTuTang()
         {
             string sql = "select * from QLYQUANNHAU.[dbo].[NhanVien]";
@@ -25,31 +26,79 @@ namespace Res_ManagementSystem.DAO
             return maTuTang;
         }
 
-        public static DataTable LayDSNhanVien()
+        public static  DataTable LayDSNhanVien()
         {
-            string sql = "select MaNV as 'Mã NV', HoTen as 'Họ Tên', NgaySinh as 'Ngày Sinh', TenDN as 'Tên ĐN', Quyen as 'Quyền' from QLYQUANNHAU.[dbo].[NhanVien]";
+            /*string sql = "select MaNV as 'Mã NV', HoTen as 'Họ Tên', NgaySinh as 'Ngày Sinh', TenDN as 'Tên ĐN', Quyen as 'Quyền' from QLYQUANNHAU.[dbo].[NhanVien]";
             DataTable dt = DataProvider.ExecuteQuery(sql);
+            return dt;*/
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[laydsnhanvien]";
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             return dt;
+            /*DataProvider myDB = new DataProvider();
+            SqlCommand command = new SqlCommand("QLYQUANNHAU.[dbo].[laydsnhanvien]", myDB.getConnection);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            return table;*/
+
         }
 
         public static DataTable LayDSNhanVienTiepTan()
         {
-            string sql = "select MaNV as 'Mã NV', HoTen as 'Họ Tên', NgaySinh as 'Ngày Sinh', TenDN as 'Tên ĐN', Quyen as 'Quyền' from QLYQUANNHAU.[dbo].[NhanVien] where Quyen = N'Tiếp Tân'";
-            DataTable dt = DataProvider.ExecuteQuery(sql);
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[LayDSNhanVienTiepTan]";
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             return dt;
         }
 
         public static DataTable LayDSNhanVienCoMK()
         {
-            string sql = "select * from QLYQUANNHAU.[dbo].[NhanVien]";
-            DataTable dt = DataProvider.ExecuteQuery(sql);
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[LayDSNhanVienCoMK]";
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             return dt;
+
         }
 
         public static string LayMatKhauTuTenDN(string TenDN)
         {
-            string sql = "select * from QLYQUANNHAU.[dbo].[NhanVien] where TenDN = N'" + TenDN + "'";
-            DataTable dt = DataProvider.ExecuteQuery(sql);
+            /*string sql = "select * from QLYQUANNHAU.[dbo].[NhanVien] where TenDN = N'" + TenDN + "'";
+            DataTable dt = DataProvider.ExecuteQuery(sql);*/
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[LayMatKhauTuTenDN]";
+            command.Parameters.AddWithValue("@TenDN", TenDN);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             string MK = dt.Rows[0][4].ToString();
             return MK;
         }
@@ -57,8 +106,18 @@ namespace Res_ManagementSystem.DAO
         public static int LayMaNVTuTenNV(string tenNV)
         {
             int maNV;
-            string sql = string.Format("select MaNV from QLYQUANNHAU.[dbo].[NhanVien] where HoTen = N'{0}'", tenNV);
-            DataTable dt = DataProvider.ExecuteQuery(sql);
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[LayMaNVTuTenNV]";
+            command.Parameters.AddWithValue("@tenNV", tenNV);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
+
             if (dt.Rows.Count > 0)
             {
                 maNV = int.Parse(dt.Rows[0][0].ToString());
@@ -72,16 +131,36 @@ namespace Res_ManagementSystem.DAO
 
         public static string LayTenNVTheoMaNV(int maNV)
         {
-            string sql = string.Format("select HoTen from QLYQUANNHAU.[dbo].[NhanVien] where MaNV = {0}", maNV);
-            DataTable dt = DataProvider.ExecuteQuery(sql);
+            /*string sql = string.Format("select HoTen from QLYQUANNHAU.[dbo].[NhanVien] where MaNV = {0}", maNV);
+            DataTable dt = DataProvider.ExecuteQuery(sql);*/
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[LayTenNVTheoMaNV]";
+            command.Parameters.AddWithValue("@maNV", maNV);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             string tenNV = dt.Rows[0]["HoTen"].ToString();
             return tenNV;
         }
 
         public static string LayQuyenNVTheoMaNV(int maNV)
         {
-            string sql = "select Quyen from QLYQUANNHAU.[dbo].[NhanVien] where MaNV = " + maNV;
-            DataTable dt = DataProvider.ExecuteQuery(sql);
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[LayQuyenNVTheoMaNV]";
+            command.Parameters.AddWithValue("@maNV", maNV);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             string quyen = dt.Rows[0]["Quyen"].ToString();
             return quyen;
         }
@@ -89,8 +168,18 @@ namespace Res_ManagementSystem.DAO
         public static bool KiemTraTenDNTonTai(string tenDN, int MaNV)
         {
             bool kq;
-            string sql = "select * from QLYQUANNHAU.[dbo].[NhanVien] where TenDN = '" + tenDN + "'" + "and TenDN <> '' and MaNV <> " + MaNV;
-            DataTable dt = DataProvider.ExecuteQuery(sql);
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[KiemTraTenDNTonTai]";
+            command.Parameters.AddWithValue("@tenDN", tenDN);
+            command.Parameters.AddWithValue("@MaNV", MaNV);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             if (dt.Rows.Count > 0)
                 kq = true;
             else
@@ -101,25 +190,86 @@ namespace Res_ManagementSystem.DAO
         public static bool ThemNhanVien(NhanVienDTO nv)
         {
             nv.MaNV = MaTuTang();
-            bool kq;
+            /*bool kq;
             string sql = string.Format("set dateformat DMY insert into QLYQUANNHAU.[dbo].[NhanVien] values ({0}, N'{1}', convert(varchar(10),'{2}',103), N'{3}', N'{4}', N'{5}')", nv.MaNV, nv.HoTen, nv.NgaySinh, nv.TenDN, nv.MatKhau, nv.Quyen);
             kq = DataProvider.ExecuteNonQuery(sql);
+            return kq;*/
+            bool kq;
+            string sql = string.Format("QLYQUANNHAU.[dbo].[ThemNhanVien]");
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = sql;
+            command.Parameters.AddWithValue("@MaNV", nv.MaNV);
+            command.Parameters.AddWithValue("@HoTen", nv.HoTen);
+            command.Parameters.AddWithValue("@NgaySinh", nv.NgaySinh);
+            command.Parameters.AddWithValue("@TenDN", nv.TenDN);
+            command.Parameters.AddWithValue("@MatKhau", nv.MatKhau);
+            command.Parameters.AddWithValue("@Quyen", nv.Quyen);
+            command.CommandType = CommandType.StoredProcedure;
+            int n = command.ExecuteNonQuery();
+            if (n > 0)
+            {
+                kq = true;
+            }
+            else
+            {
+                kq = false;
+            }
             return kq;
         }
 
         public static bool CapNhatNhanVien(NhanVienDTO nv)
         {
+            /* bool kq;
+            *//* string sql = string.Format("set dateformat DMY update QLYQUANNHAU.[dbo].[NhanVien] set HoTen = N'{0}', NgaySinh = convert(varchar(10),'{1}',103), TenDN = '{2}', MatKhau = '{3}', Quyen = N'{4}' where MaNV = {5}", nv.HoTen, nv.NgaySinh, nv.TenDN, nv.MatKhau, nv.Quyen, nv.MaNV);
+             kq = DataProvider.ExecuteNonQuery(sql);*//*
+
+             return kq;*/
             bool kq;
-            string sql = string.Format("set dateformat DMY update QLYQUANNHAU.[dbo].[NhanVien] set HoTen = N'{0}', NgaySinh = convert(varchar(10),'{1}',103), TenDN = '{2}', MatKhau = '{3}', Quyen = N'{4}' where MaNV = {5}", nv.HoTen, nv.NgaySinh, nv.TenDN, nv.MatKhau, nv.Quyen, nv.MaNV);
-            kq = DataProvider.ExecuteNonQuery(sql);
+            string sql = string.Format("QLYQUANNHAU.[dbo].[CapNhatNhanVien]");
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = sql;
+            command.Parameters.AddWithValue("@MaNV", nv.MaNV);
+            command.Parameters.AddWithValue("@HoTen", nv.HoTen);
+            command.Parameters.AddWithValue("@NgaySinh", nv.NgaySinh);
+            command.Parameters.AddWithValue("@TenDN", nv.TenDN);
+            command.Parameters.AddWithValue("@MatKhau", nv.MatKhau);
+            command.Parameters.AddWithValue("@Quyen", nv.Quyen);
+            command.CommandType = CommandType.StoredProcedure;
+            int n = command.ExecuteNonQuery();
+            if (n > 0)
+            {
+                kq = true;
+            }
+            else
+            {
+                kq = false;
+            }
             return kq;
         }
 
         public static bool XoaNhanVien(int maNV)
         {
             bool kq;
-            string sql = string.Format("delete from QLYQUANNHAU.[dbo].[NhanVien] where MaNV = {0}", maNV);
-            kq = DataProvider.ExecuteNonQuery(sql);
+            string sql = string.Format("QLYQUANNHAU.[dbo].[XoaNhanVien]");
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = sql;
+            command.Parameters.AddWithValue("@maNV", maNV);
+            command.CommandType = CommandType.StoredProcedure;
+            int n = command.ExecuteNonQuery();
+            if (n > 0)
+            {
+                kq = true;
+            }
+            else
+            {
+                kq = false;
+            }
             return kq;
         }
 
