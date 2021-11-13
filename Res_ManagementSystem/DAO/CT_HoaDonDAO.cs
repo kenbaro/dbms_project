d74b77b8777ae0a7bd1073f66aefbdd295a9c1c1
KenBaro <79701258+kenbaro@users.noreply.github.com>
2021-11-13 22:11:16 +0700

    Update CT_HoaDonDAO.cs

--- Res_ManagementSystem/DAO/CT_HoaDonDAO.cs
+++ Res_ManagementSystem/DAO/CT_HoaDonDAO.cs
@@ -1,7 +1,8 @@
-﻿using Res_ManagementSystem.DTO;
+using Res_ManagementSystem.DTO;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,14 +14,42 @@ namespace Res_ManagementSystem.DAO
         //Rút trích dữ liêu: select 
         public static DataTable LayDSCTHDTuMaHD(int maHD)
         {
-            string sql = "select TenThucDon as 'Tên TĐ', DonGia as 'Đơn Giá', SoLuong as 'Số Lượng' from QLYQUANNHAU.[dbo].[ChiTietHD] ct, QLYQUANNHAU.[dbo].[ThucDon] td where ct.MaThucDon = td.MaThucDon and ct.SoHD = " + maHD;
+
+            /*string sql = "select TenThucDon as 'Tên TĐ', DonGia as 'Đơn Giá', SoLuong as 'Số Lượng' from QLYQUANNHAU.[dbo].[ChiTietHD] ct, QLYQUANNHAU.[dbo].[ThucDon] td where ct.MaThucDon = td.MaThucDon and ct.SoHD = " + maHD;
             DataTable dt = DataProvider.ExecuteQuery(sql);
+            return dt;*/
+ 
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[LayDSCTHDTuMaHD]";
+            command.Parameters.AddWithValue("@maHD", maHD);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             return dt;
+            
         }
         public static DataTable LayDSCTHD(int SoHD)
         {
-            string sql = string.Format("select * from QLYQUANNHAU.[dbo].[ChiTietHD] where SoHD = {0}", SoHD);
+            /*string sql = string.Format("select * from QLYQUANNHAU.[dbo].[ChiTietHD] where SoHD = {0}", SoHD);
             DataTable dt = DataProvider.ExecuteQuery(sql);
+            return dt;*/
+
+            DataTable dt = new DataTable();
+            SqlConnection connect = new SqlConnection(DataProvider.connectionString());
+            connect.Open();
+            SqlCommand command = connect.CreateCommand();
+            command.CommandText = "QLYQUANNHAU.[dbo].[LayDSCTHD]";
+            command.Parameters.AddWithValue("@maHD", SoHD);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            adapter.Fill(dt);
+            connect.Close();
             return dt;
         }
         public static bool ThemChiTietHoaDon(CT_HoaDonDTO cthd)
@@ -29,6 +58,7 @@ namespace Res_ManagementSystem.DAO
             string sql = string.Format("insert into QLYQUANNHAU.[dbo].[ChiTietHD] values ({0}, {1}, {2}, {3})", cthd.SoHD, cthd.MaTD, cthd.SoLuong, cthd.DonGia);
             kq = DataProvider.ExecuteNonQuery(sql);
             return kq;
+
         }
         public static bool XoaCTHDTheoSoHD(int soHD)
         {
